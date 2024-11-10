2641e7c12a1716248b69f1f5b31bed8c3fc34c2f
Konrad Chwesiuk <46359181+FezMLG@users.noreply.github.com>
2024-11-10 23:50:39 +0100

    feat: logger user controller

--- packages/user-service/src/modules/user/user.controller.ts
+++ packages/user-service/src/modules/user/user.controller.ts
@@ -1,4 +1,4 @@
-import { Body, Controller, Delete, Get, Param, ParseIntPipe, Post, Put, Query, UsePipes } from '@nestjs/common';
+import { Body, Controller, Delete, Get, Logger, Param, ParseIntPipe, Post, Put, Query, UsePipes } from '@nestjs/common';
 import { UserService } from './user.service';
 import { CreateUserRequestDto } from './dto/create-user.request-dto';
 import { UpdateUserRequestDto } from './dto/update-user.request-dto';
@@ -17,37 +17,49 @@ export class UserController {
   private readonly DEFAULT_PER_PAGE = 25;
   private readonly DEFAULT_PAGE = 1;
 
+  private readonly logger = new Logger(UserController.name, { timestamp: true });
+
   constructor(private readonly userService: UserService) {
   }
 
   @Get()
   async getUsers(
-    @Query('perPage', ParseIntPipe) perPage?: number,
-    @Query('page', ParseIntPipe) page?: number,
+    @Query('perPage', new ParseIntPipe({ optional: true })) perPage: number = this.DEFAULT_PER_PAGE,
+    @Query('page', new ParseIntPipe({ optional: true })) page: number = this.DEFAULT_PAGE,
   ): Promise<PaginateResponse<UserEntity>> {
+    this.logger.log(this.getUsers.name, { perPage, page });
+
     return this.userService.getUsers({
-      perPage: perPage ?? this.DEFAULT_PER_PAGE,
-      page: page ?? this.DEFAULT_PAGE,
+      perPage: perPage,
+      page: page,
     });
   }
 
   @Get('/:id')
   async getUser(@Param('id', ObjectIdValidationPipe) id: string): Promise<UserResponseDto | null> {
+    this.logger.log(this.getUser.name, { id });
+
     return this.userService.getUserById(id);
   }
 
   @Post()
   async createUser(@Body() createUserDto: CreateUserRequestDto): Promise<UserResponseDto> {
+    this.logger.log(this.createUser.name, { createUserDto });
+
     return this.userService.createUser(createUserDto);
   }
 
   @Put(':id')
   async updateUser(@Param('id', ObjectIdValidationPipe) id: string, @Body() updateUserDto: UpdateUserRequestDto): Promise<UserResponseDto> {
+    this.logger.log(this.updateUser.name, { id, updateUserDto });
+
     return this.userService.updateUser(id, updateUserDto);
   }
 
   @Delete(':id')
   async deleteUser(@Param('id', ObjectIdValidationPipe) id: string): Promise<void> {
+    this.logger.log(this.deleteUser.name, { id });
+
     return this.userService.deleteUser(id);
   }
 }
